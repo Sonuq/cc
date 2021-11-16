47b5bf17dbcf3b35adb6013e66fbf28339e8b0ac
Sonuq <86333640+Sonuq@users.noreply.github.com>
2021-11-16 09:45:53 -0500

    just some fixes, you figure it out

--- packages/tnt/tnt.lua
+++ packages/tnt/tnt.lua
@@ -1,14 +1,16 @@
- local args = {...}
-local function bomb(n)
-    for i=1, n then 
+local args = {...}
+local function bomb(n, size)
+    for i=1, n do
         turtle.forward()
-        for i=1, 16 then 
+        for i=1, 16 do
             turtle.select(i)
             local item = turtle.getItemDetail()
-            if item then 
-                if item["name"] == "minecraft:tnt" then 
-                    turtle.placeDown()
-                    redstone.setOutput('bottom', 0.1)
+            if item ~= nil then 
+                if item["name"] == "minecraft:tnt" then
+                    for i=1, size do 
+                        turtle.placeDown()
+                        redstone.setOutput('bottom', true)
+                    end
                 end 
             end 
         end 
@@ -18,5 +20,5 @@ if not args[1] then
     print("use 'tnt <steps>' to place and ignite tnt")
     return 
 else
-    bomb(args[1])
+    bomb(args[1], args[2])
 end
